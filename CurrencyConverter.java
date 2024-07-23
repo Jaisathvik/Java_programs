0cbd321d32c3705d5802117c056216866961263e
Jaisathvik <jaisathvik@gmail.com>
2024-07-22 18:22:39 -0700

    Update CurrencyConverter.java

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,26 +1,20 @@
 
     
 import java. util.Scanner;
-
-/**
- * CurrencyConverter
- */
 public class CurrencyConverter {
 
     public static void main(String[] args) {
-        System.out.println("1 Ruppe");
+        System.out.println("1 Rupee");
         System.out.println("2 Dollar");
         System.out.println("3 Euro");
-        // take input
         Scanner sc = new Scanner(System.in);
         System. out.println("Choose the currency");
         int choice = sc.nextInt();
         System.out.println("Enter the amount");
         double amount = sc.nextDouble();
-        // convert the amount
         switch (choice) {
             case 1:
-                Ruppe_to_other(amount);
+                Rupee_to_other(amount);
                 break;
             case 2:
                 Dollar_to_other(amount);
@@ -34,7 +28,7 @@ public class CurrencyConverter {
 
     }
 
-public static void Ruppe_to_other(double amt) {
+public static void Rupee_to_other(double amt) {
     System.out.println("1 Ruppe = " + 0.013 + " Dollar");
     System.out.println();
 

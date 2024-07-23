8fdefa3ef48c6e75e1512de57962b94359c40086
Jaisathvik <jaisathvik@gmail.com>
2024-07-22 18:20:54 -0700

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,15 @@
+
+public class triangle {
+public static void main(String[] args) {
+
+    int row = 3;
+    for(int i=0;i<row;i++) {
+        for(int j=row-i-1;j>=1;j--)
+        System.out.print(" ");
+        for (int j=0;j<=i;j++)
+        System.out.print("* ");
+        System.out.println("");
+    
+    }
+}    
+}
